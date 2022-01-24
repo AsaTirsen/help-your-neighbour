cabffd3098c7f03ce3cd48d1f1832a4bd9c02042
Asa Tirsen <asa.tirsen@gmail.com>
2022-01-24 14:19:55 +0100

    added request details and css

--- components/requests/RequestItem.js
+++ components/requests/RequestItem.js
@@ -1,15 +1,21 @@
 //component for displaying info about each request
-
+import { useRouter } from "next/router";
 
 function RequestItem(props) {
+  const router = useRouter();
 
+    function showDetailsHandler() {
+        // router objects has method for pushing page via path 
+      router.push('/' + props.id)
+  }
   return (
-      <li>
-        <div>
+    <li>
+      <div>
         <p>Namn: {props.first_name + " " + props.last_name}</p>
         <p>Område: {props.location}</p>
         <p>Period: {props.dates}</p>
       </div>
+      <div><button onClick={showDetailsHandler}>Visa detaljer</button></div>
     </li>
   );
 }
---
+++ pages/[requestid]/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import RequestDetail from "../../components/requests/RequestDetail";
+
+function RequestDetails() {
+    return (
+        <RequestDetail
+            details="Vill ha kattvakt"
+        />
+    );
+}
+
+export default RequestDetails;
